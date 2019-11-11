aaefa0cd85d08a518f441579d85289e651b49d59
Pascal <pascalpomme@live.fr>
2019-11-11 17:24:56 -0500

    un peu mieu

--- main.py
+++ main.py
@@ -1,34 +1,32 @@
 import argparse
+from api import *
 
 def analyser_commande():
     parser = argparse.ArgumentParser(description='Donne un nom au joueur')
     parser.add_argument('idul', help='un nom pour le joueur')
-    parser.add_argument('-l', '--lister', action='store_true' , help='Lister les identifiants de vos 20 dernières parties.')
+    parser.add_argument('-l', '--lister', action='store_true', help='Lister les identifiants de vos 20 dernières parties.')
     args = parser.parse_args()
     return args
 
 if __name__ == "__main__":
     print(analyser_commande())
 
-def afficher_damier_ascii(dict):
-    deb = 'Légende: 1='+str(dict['joueurs'][0]['nom'])+' 2=automate'+'\n'+'   '+35*'-'+'\n'
+def afficher_damier_ascii(dico):
+    deb = 'Légende: 1='+str(dico['joueurs'][0]['nom'])+' 2=automate'+'\n'+'   '+35*'-'+'\n'
     sui = ''
     for i in range(8):
         sui += str(9-i)+' | '+8*'.   '+'. |'+'\n'+'  |                                   |'+'\n'
-    fin = '1 | .   .   .   .   .   .   .   .   . |'+'\n'+'--|-----------------------------------'+'\n'+'  | 1   2   3   4   5   6   7   8   9'
+    fin = '1 |' + ' .  '*8 + '. |'+'\n'+'--|' + '-'*35 + '\n' + '  | 1   2   3   4   5   6   7   8   9'
     tot = list(sui+fin)
-    for j in range(len(dict)):
-        tot[40*(18-2*dict['joueurs'][j]['pos'][1])+4*dict['joueurs'][j]['pos'][0]] = str(j+1)
-    for i in dict['murs']['horizontaux']:
-        for b in range(7):
-            tot[40*(19-2*i[1])+4*i[0]-1+b] = '-'
-    for l in dict['murs']['verticaux']:
-        for c in range(3):
-            tot[40*(18-c-2*l[1])+4*l[0]-2] = '|'
+    for j in range(len(dico)):
+        tot[40*(18-2*dico['joueurs'][j]['pos'][1])+4*dico['joueurs'][j]['pos'][0]] = str(j+1)
+    for i in dico['murs']['horizontaux']:
+        for ading in range(7):
+            tot[40*(19-2*i[1])+4*i[0]-1+ading] = '-'
+    for place in dico['murs']['verticaux']:
+        for adding in range(3):
+            tot[40*(18-adding-2*place[1])+4*place[0]-2] = '|'
     return deb + ''.join(tot)
-
-from api import *
-
 
 if analyser_commande().lister:
     print(lister_partie(analyser_commande().idul))
@@ -36,12 +34,12 @@ else:
     IDUL = analyser_commande().idul
     v = debuter_partie(IDUL)
     print(afficher_damier_ascii(v['état']))
-    id = (v['id'])
+    iden = (v['id'])
     v = 0
     while v < 1:
         b = input('type de coup ? D, MH ou MV ')
         c = input('point ? (x, y) ')
-        a = jouer_coup(id, b, c)
+        a = jouer_coup(iden, b, c)
         if a.get('message'):
             print(a)
         elif a.get('gagnant'):
